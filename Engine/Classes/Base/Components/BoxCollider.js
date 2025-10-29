ab7ace4723beb78aeb372e405f33ac032f19c568
Enoxboo <matteo.marquant@ynov.com>
2025-10-29 16:06:26 +0100

    refactor(BoxCollider): uniformize naming conventions

--- Engine/Classes/Base/Components/BoxCollider.js
+++ Engine/Classes/Base/Components/BoxCollider.js
@@ -1,16 +1,26 @@
-import {Size_2D} from "/Engine/Classes/Base/MicroClasses/Size_2D.js";
+import {Size_2D}        from "/Engine/Classes/Base/MicroClasses/Size_2D.js";
 import {Coordinates_2D} from "/Engine/Classes/Base/MicroClasses/Coordinates_2D.js";
-import {WGComponent} from "/Engine/Classes/Base/Components/WGComponent.js";
+import {WGComponent}    from "/Engine/Classes/Base/Components/WGComponent.js";
 
+/**
+ * Component that defines a rectangular collision box for an object.
+ * Used by PhysicService to detect and resolve collisions.
+ */
 class BoxCollider extends WGComponent {
     #hitbox = new Size_2D();
     #offset = new Coordinates_2D();
     #collisionGroup = "Base";
 
+    /**
+     * @returns {string} The collision group name (used for collision filtering)
+     */
     get collisionGroup() {
         return this.#collisionGroup;
     }
 
+    /**
+     * @param {string} value - The collision group name
+     */
     set collisionGroup(value) {
         if (typeof value !== 'string') {
             throw new TypeError("Collision group must be a string");
@@ -18,10 +28,16 @@ class BoxCollider extends WGComponent {
         this.#collisionGroup = value;
     }
 
+    /**
+     * @returns {Size_2D} The dimensions of the collision box
+     */
     get hitbox() {
         return this.#hitbox;
     }
 
+    /**
+     * @returns {Coordinates_2D} Position offset of the hitbox relative to the object
+     */
     get offset() {
         return this.#offset;
     }
