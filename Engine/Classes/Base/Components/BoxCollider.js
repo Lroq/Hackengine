6cf38f2b46a833b971ea765517e9282ec003f023
Enoxboo <matteo.marquant@ynov.com>
2025-10-29 15:37:30 +0100

    refactor(BoxCollider): uniformize naming convention and add validation

--- Engine/Classes/Base/Components/BoxCollider.js
+++ Engine/Classes/Base/Components/BoxCollider.js
@@ -1,27 +1,29 @@
-import {Size_2D}        from "/Engine/Classes/Base/MicroClasses/Size_2D.js";
+import {Size_2D} from "/Engine/Classes/Base/MicroClasses/Size_2D.js";
 import {Coordinates_2D} from "/Engine/Classes/Base/MicroClasses/Coordinates_2D.js";
-import {WGComponent}    from "/Engine/Classes/Base/Components/WGComponent.js";
+import {WGComponent} from "/Engine/Classes/Base/Components/WGComponent.js";
 
-class BoxCollider extends WGComponent{
-    #Hitbox = new Size_2D();
-    #Offset = new Coordinates_2D();
-    #CollisionGroup = "Base";
+class BoxCollider extends WGComponent {
+    #hitbox = new Size_2D();
+    #offset = new Coordinates_2D();
+    #collisionGroup = "Base";
 
-    get collisionGroup(){
-        return this.#CollisionGroup;
+    get collisionGroup() {
+        return this.#collisionGroup;
     }
 
-    set collisionGroup(value){
-        this.#CollisionGroup = value;
-        return this;
+    set collisionGroup(value) {
+        if (typeof value !== 'string') {
+            throw new TypeError("Collision group must be a string");
+        }
+        this.#collisionGroup = value;
     }
 
-    get hitbox(){
-        return this.#Hitbox;
+    get hitbox() {
+        return this.#hitbox;
     }
 
-    get offset(){
-        return this.#Offset;
+    get offset() {
+        return this.#offset;
     }
 }
 
