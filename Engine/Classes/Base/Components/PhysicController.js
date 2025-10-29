b266270a95545d7641b469da90479c8e923274ea
Enoxboo <matteo.marquant@ynov.com>
2025-10-29 16:09:25 +0100

    refactor(PhysicController): uniformize naming conventions

--- Engine/Classes/Base/Components/PhysicController.js
+++ Engine/Classes/Base/Components/PhysicController.js
@@ -1,21 +1,34 @@
-import {WGComponent} from "/Engine/Classes/Base/Components/WGComponent.js";
+import {WGComponent}    from "/Engine/Classes/Base/Components/WGComponent.js";
 import {Coordinates_2D} from "/Engine/Classes/Base/MicroClasses/Coordinates_2D.js";
 
+/**
+ * Component that handles physics simulation for an object.
+ * Controls velocity and gravity behavior.
+ */
 class PhysicController extends WGComponent {
     #velocity = new Coordinates_2D();
     #gravityEnabled = true;
 
+    /**
+     * @returns {Coordinates_2D} Current velocity vector (pixels per tick)
+     */
     get velocity() {
         return this.#velocity;
     }
 
+    /**
+     * @returns {boolean} Whether gravity affects this object
+     */
     get gravityEnabled() {
         return this.#gravityEnabled;
     }
 
+    /**
+     * @param {boolean} value - Enable or disable gravity
+     */
     set gravityEnabled(value) {
-        if (typeof value !== 'boolean') {
-            throw new TypeError("Gravity enabled must be a boolean value");
+        if (typeof value !== "boolean") {
+            throw new TypeError("gravityEnabled must be a boolean");
         }
         this.#gravityEnabled = value;
     }
