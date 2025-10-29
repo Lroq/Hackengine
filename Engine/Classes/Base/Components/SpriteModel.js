2cc9f14e9e72555f1ff8dd518690102c1070929c
Enoxboo <matteo.marquant@ynov.com>
2025-10-29 15:57:42 +0100

    refactor(SpriteModel): uniformize naming convention and add validation

--- Engine/Classes/Base/Components/SpriteModel.js
+++ Engine/Classes/Base/Components/SpriteModel.js
@@ -3,41 +3,45 @@ import {Coordinates_2D} from "/Engine/Classes/Base/MicroClasses/Coordinates_2D.j
 import {Size_2D} from "../MicroClasses/Size_2D.js";
 import {Utils} from "../Services/Utilities/Utils.js";
 
-class SpriteModel extends WGComponent{
-    #Sprite = new Image();
-    #SpriteOffset = new Coordinates_2D(0,0);
-    #Rotation = 1;
-    #Size = new Size_2D(0,0);
+class SpriteModel extends WGComponent {
+    #sprite = new Image();
+    #spriteOffset = new Coordinates_2D(0, 0);
+    #rotation = 1;
+    #size = new Size_2D(0, 0);
 
     constructor() {
         super();
         this.sprite = Utils.createSprite("/Engine/Assets/texture_not_found.png");
     }
 
-    get rotation(){
-        return this.#Rotation;
+    get rotation() {
+        return this.#rotation;
     }
 
-    set rotation(Rot){
-        this.#Rotation = Rot;
-    }
-
-    get sprite(){
-        return this.#Sprite;
-    }
-
-    set sprite(sprite_img){
-        if (sprite_img instanceof Image){
-            this.#Sprite = sprite_img
+    set rotation(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError("Rotation must be a number (1 or -1)");
         }
+        this.#rotation = value;
     }
 
-    get spriteOffset(){
-        return this.#SpriteOffset;
+    get sprite() {
+        return this.#sprite;
     }
 
-    get size(){
-        return this.#Size;
+    set sprite(value) {
+        if (!(value instanceof Image)) {
+            throw new TypeError("Sprite must be an Image object");
+        }
+        this.#sprite = value;
+    }
+
+    get spriteOffset() {
+        return this.#spriteOffset;
+    }
+
+    get size() {
+        return this.#size;
     }
 }
 
