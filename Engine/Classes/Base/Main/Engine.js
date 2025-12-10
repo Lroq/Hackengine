413987620f9cbe8c10221e62d76c0b03d68d6d25
Enoxboo <matteo.marquant@ynov.com>
2025-12-10 16:06:58 +0100

    feat(tutorial): add interactable tv

--- Engine/Classes/Base/Main/Engine.js
+++ Engine/Classes/Base/Main/Engine.js
@@ -86,6 +86,8 @@ class Engine {
 
             this.#LastTick = CurrentTick;
             MegaTicks.updateTicks(DeltaTime);
+
+            this.#Services.InputService.updatePreviousInputs();
         } else {
             console.warn("No 'SceneService' Detected.");
             debugger;
